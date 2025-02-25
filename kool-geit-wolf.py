49a725eb543f6eb5f13315df9fa117190e0d2322
Carlo van der Stoep <carlo.vanderstoep@gmail.com>
2025-02-25 17:43:26 +0100

    extra puzzle: kool-geit-wolf

--- kool-geit-wolf.py
+++ kool-geit-wolf.py
@@ -1,7 +1,9 @@
 from collections import deque
 
+
 def sort_string(string: str) -> str:
     return ''.join(sorted(string))
+
 
 def is_valid(left, right) -> bool:
     if 'B' in left:
@@ -16,21 +18,26 @@ def is_valid(left, right) -> bool:
 
     return True
 
+
 left = 'BGKW'
 right = ''
 visited = set()
 queue = deque([(0, left, right, [])])
+
 
 def add_queue(steps, left, right, path):
     if (left, right) not in visited and is_valid(left, right):
         queue.append((steps + 1, left, right, path))
         visited.add((left, right))
 
+
 while queue:
     steps, left, right, path = queue.popleft()
+    # print(steps, left, right)
     if left == '' and right == 'BGKW':
         print(f'Solution found in {steps} steps')
         print('Steps taken:')
+        print('BGKW:')
         for step in path:
             print(step)
         break
@@ -41,16 +48,20 @@ while queue:
         for el in left:
             new_left = sort_string(left.replace(el, ''))
             new_right = sort_string(right + 'B' + el)
-            add_queue(steps, new_left, new_right, path + [(left + 'B -> ' + new_right)])
+            add_queue(steps, new_left, new_right, path + [(new_left + f' |B{el}-> ' + new_right)])
+
+        # move boat only
         new_left = sort_string(left.replace('B', ''))
         new_right = sort_string(right + 'B')
-        add_queue(steps, new_left, new_right, path + [(left + 'B -> ' + new_right)])
+        add_queue(steps, new_left, new_right, path + [(new_left + f' |B-> ' + new_right)])
     else:
         right = right.replace('B', '')
         for el in right:
             new_left = sort_string(left + 'B' + el)
             new_right = sort_string(right.replace(el, ''))
-            add_queue(steps, new_left, new_right, path + [(right + 'B -> ' + new_left)])
+            add_queue(steps, new_left, new_right, path + [(new_left + f' <-B{el}| ' + new_right)])
+
+        # move boat only
         new_left = sort_string(left + 'B')
         new_right = sort_string(right.replace('B', ''))
-        add_queue(steps, new_left, new_right, path + [(right + 'B -> ' + new_left)])
+        add_queue(steps, new_left, new_right, path + [(new_left + f' <-B| ' + new_right)])
